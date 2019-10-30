c7d4ce71feabda32e4877ae4616b6d8db63eb271
Dominique Maack <dominiquemaack@Dominiques-MacBook-Pro.local>
2019-10-30 15:18:08 -0600

    started in post file and need to start in user in order to see if logic is working in post -- will come back to later

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,13 +1,31 @@
 const express = require('express');
-
+const db = require('./postDb.js')
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+    db.get()
+    .then(posts => {
+        res.status(200).json(posts)
+    })
+    .catch(() => {
+        res.status(500).json({ error: "The posts information could not be retrieved." })
+    })
 });
 
 router.get('/:id', (req, res) => {
-
+    const id = req.params.id
+    
+    db.getById(id)
+    .then(post => {
+        if(post) {
+            res.status(200).json(post)
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }
+    })
+    .catch(() => {
+        res.status(500).json({ error: "The post information could not be retrieved." })
+    })
 });
 
 router.delete('/:id', (req, res) => {
