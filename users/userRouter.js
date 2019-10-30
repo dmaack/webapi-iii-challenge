875340fcdcee02fd8cb8ac6a1a410059836de6b7
Dominique Maack <dominiquemaack@Dominiques-MacBook-Pro.local>
2019-10-30 16:26:00 -0600

    validateUser middleware working

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const userDb = require('./userDb.js');
+const postDb = require('../posts/postDb.js')
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, (req, res) => {
+    const name = req.body
 
+    userDb.insert(name)
+    .then(item => {
+        res.status(201).json(item)
+    })
+    .catch(() => {
+        res.status(500).json({ error: "The posts information could not be retrieved." })
+    })
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -37,7 +47,18 @@ function validateUserId(req, res, next) {
 };
 
 function validateUser(req, res, next) {
+    console.log(`\n === validate user console ====\n`)    
+    const name = req.body.name
+    console.log('name please', name)
 
+    if(!req.body) {
+        res.status(400).json({ message: "missing user data" })
+    } 
+    if(!name) {
+        res.status(400).json({ message: "missing required name field" })
+    } else {
+        next();
+    }
 };
 
 function validatePost(req, res, next) {
