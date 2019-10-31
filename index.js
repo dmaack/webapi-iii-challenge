37294706c4e4d94ae3335b39b4d452ac2d5fe433
Dominique Maack <dominiquemaack@Dominiques-MacBook-Pro.local>
2019-10-31 11:59:23 -0600

    added start script and env fille

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+require('dotenv').config()
 const server = require('./server')
 
-const port = 8020;
+
+const port = process.env.PORT || 8000;
+// const port = 8020;
 server.listen(port, () => console.log(`Listening on port ${port}`))
